9cc026ca5fef6eade22fc251b2cabeb085ba8c2b
ederdon <ederpalacin@gmail.com>
2022-03-15 18:32:48 +0000

    primera prueba

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domainsName = "";
+
+const domainGenerator = (arrayP, arrayA, arrayN) => {
+  for (pronouns of arrayP) {
+    for (adjetives of arrayA) {
+      for (nouns of arrayN) {
+        domainsName += pronouns + adjetives + nouns + "<br>";
+      }
+    }
+  }
+  document.getElementById(domainNames).innerHTML = domainsName;
 };
+
+window.onload = domainGenerator;
+
+domainGenerator(pronoun, adj, noun);
