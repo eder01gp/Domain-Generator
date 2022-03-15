59c904813f3c50cdb9cd9fe08edbcac3ef96a3c0
ederdon <ederpalacin@gmail.com>
2022-03-15 18:49:12 +0000

    version funcional

--- src/app.js
+++ src/app.js
@@ -1,26 +1,23 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
+let ending = [".com", ".io", ".net", ".us"];
 let domainsName = "";
 
-const domainGenerator = (arrayP, arrayA, arrayN) => {
-  for (pronouns of arrayP) {
-    for (adjetives of arrayA) {
-      for (nouns of arrayN) {
-        domainsName += pronouns + adjetives + nouns + "<br>";
+const domainGenerator = (arrayP, arrayA, arrayN, arrayE) => {
+  for (let pronouns of arrayP) {
+    for (let adjetives of arrayA) {
+      for (let nouns of arrayN) {
+        for (let ends of arrayE)
+          domainsName += pronouns + adjetives + nouns + ends + "<br>";
       }
     }
   }
-  document.getElementById(domainNames).innerHTML = domainsName;
+  document.getElementById("domainNames").innerHTML = domainsName;
 };
 
-window.onload = domainGenerator;
+window.onload = domainGenerator(pronoun, adj, noun, ending);
 
 domainGenerator(pronoun, adj, noun);
